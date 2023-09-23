6832b9a094af6acd618aed00959dc3e390f56030
leehomeecky <meecky4chist@gmail.com>
2023-09-23 05:01:22 +0100

    fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,16 @@
 # Create a file in /tmp
 
-file { 'school':
+# checking if the temp file exists
+file { '/tmp':
+  ensure => 'directory',
+}
+
+file { '/tmp/school':
   ensure  => 'present',
   content => 'I love Puppet',
   group   => 'www-data',
   mode    => '0744',
   owner   => 'www-data',
   path    => '/tmp/school',
+  require => File['/tmp'],
 }
