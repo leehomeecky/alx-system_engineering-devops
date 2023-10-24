ff08eb66cddd2e47b63346736583a12227e265eb
leehomeecky <meecky4chist@gmail.com>
2023-10-24 21:19:34 +0100

    fix

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,51 +3,28 @@
 for a given employee ID,
 returns information about his/her TODO list progress.
 """
+import csv
 import requests
 import sys
-import csv
 
 
-def fetchdata(employeeid):
+def fetchdata(user_id):
     """ Write a Python script that, using this REST API,
     for a given employee ID,
     returns information about his/her TODO list progress.
     """
-    if employeeid is None:
+    if user_id is None:
         return
-    baseurl = f'https://jsonplaceholder.typicode.com/todos?userId={employeeid}'
-    usersurl = f'https://jsonplaceholder.typicode.com/users/{employeeid}'
-    user_rep = requests.get(usersurl)
-    user_data = user_rep.json()
-    if user_rep.status_code != 200:
-        return
-    base_rep = requests.get(baseurl)
-    base_data = base_rep.json()
-    tott = [task for task in base_data]
-    username = user_data['name']
-    userid = user_data['id']
-    # Print the result
-    dict = {}
-    list = []
-    for task in tott:
-        dict['USER_ID'] = task["userId"]
-        dict['USERNAME'] = username
-        dict['TASK_COMPLETED_STATUS'] = task['completed']
-        dict['TASK_TITLE'] = task["title"]
-        list.append(dict)
-        dict = {}
-    # Now, let's save the data to a CSV file
-    filename = f'{userid}.csv'
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    tott = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open(filename, 'w', newline='') as csvfile:
-        fieldnames = ['USER_ID', 'USERNAME',
-                      'TASK_COMPLETED_STATUS', 'TASK_TITLE']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
-                                quoting=csv.QUOTE_ALL, delimiter=',')
-        # writer.writeheader()  # Write the CSV header
-        # Write each dictionary as a CSV row
-        for row in list:
-            writer.writerow(row)
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+                ) for t in tott]
 
 
 if __name__ == '__main__':
